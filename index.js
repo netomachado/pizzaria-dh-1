05b61f24ef4913b4f2b3394aa5d22a82d44d3520
muriloignaccio <murilohsignacio@gmail.com>
2021-05-28 21:21:19 -0300

    Criando a rota de listar pizzas

--- index.js
+++ index.js
@@ -1,22 +1,13 @@
 const express = require("express");
 const app = express();
 
+app.use(express.json());
+
 const pizzas = require("./database/pizzas.json");
 
-const listarTodasAsPizzas = () => {
-  let conteudo = "";
+app.get("/pizzas", (req, res) => res.json(pizzas));
 
-  pizzas.forEach((pizza) => {
-    conteudo += `
-      id: ${pizza.id}
-      Sabor: ${pizza.sabor}
-      Categoria: ${pizza.categoria}
-      Preço: ${pizza.preco}
-    `;
-  });
-
-  return conteudo;
-};
+// *DESAFIO* Criar uma rota para criar uma nova pizza, utilizem o método http correto, adicione no array de pizzas e retorne essa nova pizza como json. IMPORTANTE: a nova pizza precisa ter id
 
 const adicionarPizza = function (sabor, categoria, preco) {
   const pizzaNova = {
@@ -30,11 +21,6 @@ const adicionarPizza = function (sabor, categoria, preco) {
 
   console.log(`A pizza de ${sabor} foi adicionada com sucesso!`);
 };
-
-adicionarPizza("Presunto", "Salgado", 25);
-adicionarPizza("Catupiry", "Vegetariana", 40);
-
-console.log(listarTodasAsPizzas());
 
 const procurarPizzaPeloNome = function (nomePizza) {
   const pizzaEncontrada = pizzas.find((pizza) => pizza.sabor === nomePizza);
